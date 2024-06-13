5845cde0ca34774873c8a0628f7cf8b059f30200
irismake <iris3455@gmail.com>
2024-06-13 15:07:10 +0900

    fix : Change api request origin

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "0d5305042ffe92082ac2eadeed5b6fec",
-"index.html": "e4364684a2e64ddb857b721c50f9176c",
-"/": "e4364684a2e64ddb857b721c50f9176c",
-"main.dart.js": "501493e55c76005b1e8babb6efe556b4",
+"index.html": "f87de5cd6fa801b62a03f48a1aa457d6",
+"/": "f87de5cd6fa801b62a03f48a1aa457d6",
+"main.dart.js": "3f5d5fcff170aba33ef88b9788674b02",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
